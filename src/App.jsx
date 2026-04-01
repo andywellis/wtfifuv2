f271394bf2903c21fe532d68db4f94d5cd4fcfb7
andywellis <andy.w.ellis@gmail.com>
2026-03-31 22:21:11 -0700

    delete pharmacology id

--- src/App.jsx
+++ src/App.jsx
@@ -165,10 +165,5 @@ export default function App() {
       }
     </div>
   );
-  {
-    id: "pharmacology", name: "Pharmacology", icon: "💊", color: "#f472b6", bg: "#2e0f1a",
-    subtitle: "Perioperative meds, anticoagulation, MRONJ, GLP-1/SGLT2, drug interactions, epi limits",
-    filterGroups: ["Anticoagulants & Antiplatelets", "Antihypertensives", "Diabetes Medications", "Bisphosphonates & MRONJ", "Psychiatric Medications", "Corticosteroids", "Supplements & Cannabinoids", "Other Medication Classes", "Quick Reference"],
-    dataKey: "pharmacology", ready: true,
-  },
+  }
 
